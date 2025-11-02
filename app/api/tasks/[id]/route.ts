969152a077127a8e0495a4c3b4476d5e4f987cc8
cypherab01 <aghimire074@gmail.com>
2025-11-02 20:05:17 +0545

    build: typescript

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -4,7 +4,7 @@ import prisma from "@/lib/prisma";
 
 export async function PATCH(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id } = await params;
@@ -52,7 +52,7 @@ export async function PATCH(
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id } = await params;
